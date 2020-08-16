8d25d708ed8b5c5739f9df70471c0cd9251ebd1a
Joshua Fazel <fazeljoshua@gmail.com>
2020-08-16 05:57:07 +0530

    Added Check if player is grounded, cleaned up code

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,34 +1,73 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+﻿using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     [Header("Player")]
     [SerializeField] float _moveSpeed = 10f;
     [SerializeField] float _jumpSpeed = 10f;
-    //[SerializeField] bool _normalWorld;
+    [SerializeField] LayerMask _platformLayerMask;
     
-    //InvertedUI _invertedUI;
-    //NormalUI _normalUI;
-    Rigidbody2D _rigidBody;
-    SpriteRenderer _spriteRenderer;
-    GameManager _gameManager;
-
-    //private void UpdateWorldFlag() => _normalWorld = _normalWorld ? false : true;
+    private Rigidbody2D _rigidBody;
+    private SpriteRenderer _spriteRenderer;
+    private GameManager _gameManager;
+    private BoxCollider2D _boxCollider;
+    private bool _jump;
 
     private void Start()
     {
-        //_invertedUI = FindObjectOfType<InvertedUI>();
-        //_normalUI = FindObjectOfType<NormalUI>();
+        CacheData();
+        UpdatePlayerSprite();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+        JumpAndInvert();
+    }
+
+    private void Update()
+    {
+        ReadInput();
+    }
+
+    private void CacheData()
+    {
         _rigidBody = GetComponent<Rigidbody2D>();
+        _boxCollider = GetComponent<BoxCollider2D>();
         _spriteRenderer = GetComponent<SpriteRenderer>();
         _gameManager = FindObjectOfType<GameManager>();
+    }
 
-        SetActiveWorldGameobjects();
-        UpdatePlayerSprite();
+    private bool IsGrounded()
+    {
+        float extraHeight = 0.1f;
+        RaycastHit2D raycatHit = Physics2D.BoxCast(_boxCollider.bounds.center, _boxCollider.bounds.size, 0f,
+                                                    Vector2.down, extraHeight, _platformLayerMask);
+
+        //-------------Code for debugging only
+        //Color raycolor;
+        //if (raycatHit.collider != null)
+        //{
+        //    raycolor = Color.green;
+        //}
+        //else
+        //{
+        //    raycolor = Color.red;
+        //}
+        //Debug.DrawRay(_boxCollider.bounds.center + new Vector3(_boxCollider.bounds.extents.x, 0), Vector2.down * (_boxCollider.bounds.extents.y + extraHeight), raycolor);
+        //Debug.DrawRay(_boxCollider.bounds.center - new Vector3(_boxCollider.bounds.extents.x, 0), Vector2.down * (_boxCollider.bounds.extents.y + extraHeight), raycolor);
+        //Debug.DrawRay(_boxCollider.bounds.center - new Vector3(_boxCollider.bounds.extents.x, _boxCollider.bounds.extents.y + extraHeight), Vector2.right * (_boxCollider.bounds.extents.x * 2f), raycolor);
+        //-------Can remove after playtesting
+
+        return raycatHit.collider != null;
+    }
+
+    private void ReadInput()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        {
+            _jump = true;
+        }
     }
 
     private void UpdatePlayerSprite()
@@ -43,69 +82,32 @@ public class Player : MonoBehaviour
         //}
     }
 
-    //use this for physics related elements
-    private void FixedUpdate()
+    private void JumpAndInvert()
     {
-        //Invert();
-        Move();
-        Jump();
-    }
+        if (_jump)
+        {
+            Jump();
+            Invert();
 
-    private void Update()
-    {
-        
-    }
-    
-    private void SetActiveWorldGameobjects()
-    {
-        //if (_normalWorld)
-        //{
-        //    _invertedUI.gameObject.SetActive(false);
-        //    _normalUI.gameObject.SetActive(true);
-        //}
-        //else
-        //{
-        //    _invertedUI.gameObject.SetActive(true);
-        //    _normalUI.gameObject.SetActive(false);
-        //}
-    }
-
-    private void Invert()
-    {
-        //if(Input.GetKeyDown(KeyCode.Space))
-        //{
-            //Debug.Log("Space was pressed. World should be inverted");
-
-            //UpdateWorldFlag();
-            //SetActiveWorldGameobjects();
-            //print("returning enumworldtype from Next World: " + _gameManager.NextWorld());
-            _gameManager.NextWorld();
-            //print("white world type:" + WORLD_TYPE.WHITE);
-            //print("black world type:" + WORLD_TYPE.BLACK);
-            UpdatePlayerSprite();
-            //Jump();
-        //}
+            Debug.Log("Player was grounded and jumped. World should be inverted");
+        }
     }
 
     private void Jump()
     {
-        if (Input.GetKeyDown(KeyCode.Space)) {
-            if (Mathf.Approximately(_rigidBody.velocity.y, Mathf.Epsilon))
-            {
-                //GetComponent<Rigidbody2D>().velocity = new Vector2(0, _jumpSpeed);
-                GetComponent<Rigidbody2D>().AddForce(new Vector2(0, _jumpSpeed), ForceMode2D.Impulse);
-            }
-
-            Debug.Log("Space was pressed. World should be inverted");
-            Invert();
-        }
-        
+        _rigidBody.AddForce(new Vector2(0, _jumpSpeed), ForceMode2D.Impulse);
+        _jump = false;
     }
 
     private void Move()
     {
-        var deltaX = Input.GetAxis("Horizontal")  * Time.deltaTime * _moveSpeed;
+        var movementX = Input.GetAxis("Horizontal")  * Time.deltaTime * _moveSpeed;
+        transform.position = new Vector2(transform.position.x + movementX, transform.position.y);
+    }
 
-        transform.position = new Vector2(transform.position.x + deltaX, transform.position.y);
+    private void Invert()
+    {
+        _gameManager.LoadNextWorld();
+        UpdatePlayerSprite();
     }
 }
